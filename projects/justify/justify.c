8ea1afd1b9c7ac6512d037cb0fc8214c2906eff4
Hendrix Gryspeerdt <95045551+hendrixgg@users.noreply.github.com>
2023-08-15 10:58:24 -0400

    wrote a bit of pseudo code

--- projects/justify/justify.c
+++ projects/justify/justify.c
@@ -1,18 +1,28 @@
-
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
     int line_width = 80;
-    char *buffer = NULL;
     /*
-    parse the command line arguments, change the default line width if it was specified
-
+    parse the command line arguments,
+    change the default line width if it was specified
+    */
+    char *buffer = malloc(line_width * sizeof(char));
+    /*
     while(true) {
         read a word
 
         if (can't read word) {
+            write contents of line buffer to stdout without justification;
             break;
         }
+        if (word doesn't fit in line buffer) {
+            write contents of line buffer to stdout with justification;
+            clear line buffer;
+        }
+        add word to line buffer;
     }
+    see page 386 from book
     */
 }
