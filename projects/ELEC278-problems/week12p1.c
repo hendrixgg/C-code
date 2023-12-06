7b58dc169bea6864c71f54f8093ade756fcd41f8
Hendrix Gryspeerdt <95045551+hendrixgg@users.noreply.github.com>
2023-12-05 20:22:09 -0500

    improved graph code

--- projects/ELEC278-problems/week12p1.c
+++ projects/ELEC278-problems/week12p1.c
@@ -3,7 +3,7 @@
 #include <assert.h>
 #include <stdlib.h>
 
-// Undirected Graph
+// Undirected Graph. No weights on edges. No self-edges.
 struct graph
 {
     struct vertex_node *vertices_head;
@@ -26,12 +26,17 @@ struct edge_node
     struct edge_node *next;
 };
 
+/**
+ * Adds a vertex to the graph.
+ *
+ * @param g The graph to add the vertex to.
+ * @param data The data to store in the vertex.
+ * @return True if the vertex was added, false if it already existed.
+ */
 bool add_vertex(struct graph *g, int data)
 {
     assert(g != NULL);
-    // TODO: add a vertex, or return false if it already exists.
     // Allocate a new vertex node in g->vertices_head.
-    // Set the data field to data.
 
     // put this node at the end of the list stored in g.
     // this ensures that there are no duplicates.
@@ -77,7 +82,6 @@ bool add_edge(struct graph *g, int v1, int v2)
     // Make sure that v1 and v2 are not already connected. if so, return false.
     // Start by traversing the list of edges for v1_node.
     struct edge_node *curr_edge = v1_node->edges_head;
-    // Ensure that the edge is not already in the list of edges for v1_node.
     while (curr_edge != NULL)
     {
         // If the other node is v2_node, then they are already connected.
@@ -97,40 +101,46 @@ bool add_edge(struct graph *g, int v1, int v2)
     return true;
 }
 
+/**
+ * Determines whether two vertices are adjacent.
+ *
+ * @param g The graph to check.
+ * @param v1 The first vertex.
+ * @param v2 The second vertex.
+ * @return True if the vertices are adjacent, false otherwise.
+ */
 bool are_adjacent(struct graph *g, int v1, int v2)
 {
+    assert(g != NULL);
     if (v1 == v2)
         return false;
 
     struct vertex_node *curr = g->vertices_head;
-    // look for v1 in the list of vertices.
-    while (curr != NULL)
-    {
-        if (curr->data == v1)
-        {
-            // look for v2 in the list of edges for v1.
-            struct edge_node *curr_edge = curr->edges_head;
-            while (curr_edge != NULL)
-            {
-                if (curr_edge->other->data == v2)
-                    return true;
-                curr_edge = curr_edge->next;
-            }
-            return false;
-        }
-        if (curr->data == v2)
-        {
-            // look for v2 in the list of edges for v1.
-            struct edge_node *curr_edge = curr->edges_head;
-            while (curr_edge != NULL)
-            {
-                if (curr_edge->other->data == v1)
-                    return true;
-                curr_edge = curr_edge->next;
-            }
-            return false;
-        }
+    // Look for v1 or v2 in the list of vertices, stop on whichever comes first.
+    while (curr != NULL && curr->data != v1 && curr->data != v2)
         curr = curr->next;
+    if (curr == NULL)
+        return false;
+    struct edge_node *curr_edge = curr->edges_head;
+    if (curr->data == v1)
+    {
+        // Look for v2 in the list of edges for v1.
+        while (curr_edge != NULL)
+        {
+            if (curr_edge->other->data == v2)
+                return true;
+            curr_edge = curr_edge->next;
+        }
+    }
+    else if (curr->data == v2)
+    {
+        // Look for v1 in the list of edges for v2.
+        while (curr_edge != NULL)
+        {
+            if (curr_edge->other->data == v1)
+                return true;
+            curr_edge = curr_edge->next;
+        }
     }
     return false;
 }
